ab00f5529c2f8ce95123fd27fd01a76e45b663cc
taozhiwei <taozhiwei@tjitech.cn>
2019-03-27 22:55:39 +0800

    rabbit第一天

---
+++ src/main/java/cn/free/myRabbitMq/MyRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package cn.free.myRabbitMq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/free/myRabbitMq/MyRabbitMqApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.free.myRabbitMq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyRabbitMqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
